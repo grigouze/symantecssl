e77a524c5c6554d721bd71ff9eda0e66ed4f4e5c
Donald Stufft <donald@stufft.io>
2014-07-10 12:16:06 -0400

    Symantec API doesn't like the CSR

--- tests/functional/test_order.py
+++ tests/functional/test_order.py
@@ -37,21 +37,21 @@ def test_order(symantec):
         approveremail="admin@testingsymantecssl.com",
         csr=textwrap.dedent("""
             -----BEGIN CERTIFICATE REQUEST-----
-            MIICzDCCAbQCAQAwYzELMAkGA1UEBhMCVVMxDjAMBgNVBAgTBVRleGFzMRQwEgYD
-            VQQHEwtTYW4gQW50b25pbzENMAsGA1UEChMEVGVzdDEfMB0GA1UEAxMWdGVzdGlu
-            Z3N5bWFudGVjc3NsLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
-            AOO5cCIKIGegzQDEK/Ng3+6EdY4H7ZFAvSsFFdDTXfckpBznm/j3sbjKv7LlnyoQ
-            SVEhhr/mErtXfe19mSPCzJ3jUSotONnKpjvbZSLn7xJ4tZzIxS5OOY/VeLQc0V7A
-            jheswmFN9+BFHMjNHEYc8KzPEe67BS2exiagwUQD+g55bQr0iZnKIYRuDQ8X5Kaq
-            ivPDfGFNCo29Mwyzo6sy6fFjqFzul/kjSmIRizaxuPT6zLKAMCLIkpxVp+YPA7tB
-            QORu+UvgTtsIerG8EM4SQC53Ctwxw+V2lH5t9UH4UtX+IEDn8pUVbgNHfrt8TPqZ
-            ynsCKdwJQ7VmgK8wwAdM7r0CAwEAAaAkMCIGCSqGSIb3DQEJDjEVMBMwEQYJYIZI
-            AYb4QgEBBAQDAgZAMA0GCSqGSIb3DQEBBQUAA4IBAQCdF9dZg6KH4VgWTJcPskCX
-            feIAc+BU+7B4eA4RS7StuABz+x6ff39R9g62IM6ppIZU3VKmb3jClD/xPjHTrsiE
-            0WgTG7iWBF6oOyqiTX5/w6KlWddaBq/m2YzhnpDb/IQJhxRgxt0alHBtlcglzPms
-            CbbHMcSlYXIr32S20rhS1oaiI0aIPjbbEjzSViFN7IIDUCXPgb0DgQXKrZZpT9fh
-            +6MlhlabtkSDrRzZXOeOFOxQBRjRlv/ZiwGhfD0u/xTnKyOU8p8XFHRFpg/zRyBf
-            M7HcFYsHVIgPB1M303pLtdN91Iig7D8LLIMDl9p+HTEpNLmAVefUeq20hPZTVee6
+            MIICpjCCAY4CAQAwYTELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAlRYMRQwEgYDVQQH
+            DAtTYW4gQW50b25pbzEOMAwGA1UECgwFTXlPcmcxHzAdBgNVBAMMFnRlc3Rpbmdz
+            eW1hbnRlY3NzbC5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDf
+            klapgfE7MDlB++19m/I8TlzGJcIoiFUhqJN2TdCoiTPA+5eHkRRY9WWIS3R4xxQG
+            fA52dxynLRtce1Sr4zxeP5HkxtWKWbIdir2YVqnjWSoqyf0+8VNX4cYhCRu3BETu
+            Dfej5xjt7EH7++BoA5kGzcwv+7jb9U73XRREuZEq0l26QTd7EZjGZYATHJvz2idv
+            Z784+iGrO0Qw76rYHCnhffWDld9lgMKXcgRJpESIDQHRsPMJSREyWAOr0Fov/z75
+            YU3n4vXIFZSeSa7fGbyLFFXMNJpu4xG6x8JufgJkGZlgCEiX8aG6YjqV2Z3LrYld
+            4jzT408Uqyw2GftzZMDZAgMBAAGgADANBgkqhkiG9w0BAQUFAAOCAQEAPxIjS7g/
+            MUfNsRYuplgHh9BbZl13SVdFXc9POSJwSCy1pQhEhM+e7izGD3po+V0TlZ5DZohT
+            djrGMEZvBm4OkwB7g/9hzEI5kyHfBXzBVn9ybcIzEMlRqAWc0tS1Kn+EyyDlbGnh
+            iFEk178Q0KTuOIZfPVBKZjji0o5gj13wrRBAxJARf/0/MRqpg3mL932QgjmSB2dL
+            /57yk0hXh1ChA8d2htKdwb3RnRJHOjVxWbWjYGcuAMz7RTEN9pWviTM3y7FfTWTP
+            Q24Nrp12Ez1cALXb5t/lZkbrCtizCjUuEpREzIRMUYnWZEa7pw/CGbSTYH4a2x7n
+            L5mReDt1ijwjGg==
             -----END CERTIFICATE REQUEST-----
         """)
     )
