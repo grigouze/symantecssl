e31cc76be1a0ee028f0a4b524b890432442e335d
Paul Glass <paul.glass@rackspace.com>
2014-07-15 16:25:46 -0500

    Added a functional test for reissue
    The test does the following:
        1. Create a new order
        2. Force approval of the order
        3. Do the reissue and verify the response
        4. Query the order to verify the cert status is PENDING_REISSUE
    
    Unfortunately, forcing approval seems to take between 5 and 15
        minutes for the API to complete.

--- tests/functional/test_order.py
+++ tests/functional/test_order.py
@@ -3,7 +3,9 @@ import pytest
 import random
 import string
 import textwrap
+import time
 
+from symantecssl.order import ModifyOperation
 from symantecssl.order import ProductCode
 from symantecssl.exceptions import SymantecError
 
@@ -104,7 +106,7 @@ def test_get_order_by_partner_order_id(symantec):
 
 
 @pytest.fixture
-def validation_kwargs():
+def order_kwargs():
     order_id = "".join(random.choice(string.ascii_letters) for _ in range(30))
     return {
         "partnerorderid": order_id,
@@ -153,25 +155,95 @@ def validation_kwargs():
     }
 
 
-def test_validate_order_parameters_success(symantec, validation_kwargs):
-    validation_kwargs["partnercode"] = symantec.partner_code
-    validation_kwargs["productcode"] = ProductCode.SSL123
+def test_validate_order_parameters_success(symantec, order_kwargs):
+    order_kwargs["partnercode"] = symantec.partner_code
+    order_kwargs["productcode"] = ProductCode.SSL123
 
-    response = symantec.validate_order_parameters(**validation_kwargs)
+    response = symantec.validate_order_parameters(**order_kwargs)
 
     expected_keys = set(["ValidityPeriod", "Price", "ParsedCSR", "RenewalInfo",
                          "CertificateSignatureHashAlgorithm"])
     assert set(response.keys()) == set(expected_keys)
 
 
-def test_validate_order_parameters_error(symantec, validation_kwargs):
-    validation_kwargs["partnercode"] = symantec.partner_code
-    validation_kwargs["productcode"] = ProductCode.SSL123
-    validation_kwargs["webservertype"] = "9999"
+def test_validate_order_parameters_error(symantec, order_kwargs):
+    order_kwargs["partnercode"] = symantec.partner_code
+    order_kwargs["productcode"] = ProductCode.SSL123
+    order_kwargs["webservertype"] = "9999"
 
     with pytest.raises(SymantecError) as e:
-        symantec.validate_order_parameters(**validation_kwargs)
+        symantec.validate_order_parameters(**order_kwargs)
 
     assert str(e).endswith("The Symantec API call ValidateOrderParameters"
                            " returned an error: 'Missing or Invalid Field:"
                            "  WebServerType'")
+
+
+def test_reissue(symantec, order_kwargs):
+    one_minute = 60
+
+    order_kwargs["partnercode"] = symantec.partner_code
+    order_kwargs["productcode"] = product_code = ProductCode.SSL123
+    order_id = order_kwargs['partnerorderid']
+
+    def query_order():
+        return symantec.get_order_by_partner_order_id(
+            partnercode=symantec.partner_code,
+            productcode=product_code,
+            partnerorderid=order_id,
+            returncertificateinfo="true",
+        )
+
+    def modify_order(operation):
+        return symantec.modify_order(
+            partnercode=symantec.partner_code,
+            productcode=product_code,
+            partnerorderid=order_id,
+            modifyorderoperation=operation,
+        )
+
+    def ensure_order_completed():
+        query_resp = query_order()
+        order_status = query_resp["OrderInfo"]["OrderStatusMajor"].lower()
+        if order_status == 'complete':
+            return
+
+        # Force approval of the order. The approval initially fails with a
+        # 'SECURITY REVIEW FAILED'. Per the API docs, do a push_order_state
+        # to force the order to COMPLETED state.
+        #
+        # For SSL123, this takes up to 15 minutes or so.
+        # For QuickSSLPremium, this happens immediately but sends an email.
+        try:
+            modify_order(ModifyOperation.Approve)
+        except SymantecError:
+            modify_order(ModifyOperation.PushState)
+
+        # wait until the order is finished; timeout after thirty minutes
+        start = time.time()
+        while True:
+            query_resp = query_order()
+            order_status = query_resp["OrderInfo"]["OrderStatusMajor"].lower()
+            if order_status == 'complete':
+                break
+            elif time.time() - start > 30 * one_minute:
+                raise Exception("Order approval timed out")
+            time.sleep(one_minute)
+
+    symantec.order(**order_kwargs)
+    ensure_order_completed()
+    reissue_resp = symantec.reissue(
+        partnercode=symantec.partner_code,
+        productcode=product_code,
+        partnerorderid=order_id,
+        reissueemail=order_kwargs["admincontactemail"],
+        csr=order_kwargs['csr'],
+    )
+
+    assert set(reissue_resp.keys()) == set(['GeoTrustOrderID',
+                                            'PartnerOrderID'])
+    assert reissue_resp['PartnerOrderID'] == order_id
+
+    query_resp = query_order()
+    cert_status = query_resp['CertificateInfo']['CertificateStatus']
+    assert cert_status == 'PENDING_REISSUE'
