3cc9ecf555cdc1732fd48f74d2a0236127ffb0d9
Kevin Bishop <kevin.bishop@rackspace.com>
2014-07-16 13:12:55 -0500

    Implemented GetOrderByPartnerId test and cleaned up copy-pasta

--- tests/functional/test_order.py
+++ tests/functional/test_order.py
@@ -10,10 +10,8 @@ from symantecssl.order import ProductCode
 from symantecssl.exceptions import SymantecError
 
 
-def test_order(symantec):
-    order_id = "".join(random.choice(string.ascii_letters) for _ in range(30))
-
-    order_data = symantec.order(
+def order_with_order_id(symantec, order_id):
+    return symantec.order(
         partnercode=symantec.partner_code,
         productcode=ProductCode.SSL123,
         partnerorderid=order_id,
@@ -60,6 +58,12 @@ def test_order(symantec):
             -----END CERTIFICATE REQUEST-----
         """)
     )
+
+
+def test_order(symantec):
+    order_id = "".join(random.choice(string.ascii_letters) for _ in range(30))
+
+    order_data = order_with_order_id(symantec, order_id)
 
     assert set(order_data.keys()) == set(["GeoTrustOrderID", "PartnerOrderID"])
     assert order_data["PartnerOrderID"] == order_id
@@ -217,54 +221,8 @@ def test_get_orders_by_date_range(symantec):
         orderids.append("".join(random.choice(string.ascii_letters)
                         for _ in range(30)))
 
-    for orderid in orderids:
-        symantec.order(
-            partnercode=symantec.partner_code,
-            productcode=ProductCode.SSL123,
-            partnerorderid=orderid,
-            organizationname="MyOrg",
-            addressline1="5000 Walzem",
-            city="San Antonio",
-            region="TX",
-            postalcode="78218",
-            country="US",
-            organizationphone="2103124000",
-            validityperiod="12",
-            serverCount="1",
-            webservertype="20",
-            admincontactfirstname="John",
-            admincontactlastname="Doe",
-            admincontactphone="2103122400",
-            admincontactemail="someone@email.com",
-            admincontacttitle="Caesar",
-            admincontactaddressline1="123 Road",
-            admincontactcity="San Antonio",
-            admincontactregion="TX",
-            admincontactpostalcode="78218",
-            admincontactcountry="US",
-            techsameasadmin="True",
-            billsameastech="True",
-            approveremail="admin@testingsymantecssl.com",
-            csr=textwrap.dedent("""
-                -----BEGIN CERTIFICATE REQUEST-----
-                MIICpjCCAY4CAQAwYTELMAkGA1UEBhMCVVMxCzAJBgNVBAgMAlRYMRQwEgYDVQQH
-                DAtTYW4gQW50b25pbzEOMAwGA1UECgwFTXlPcmcxHzAdBgNVBAMMFnRlc3Rpbmdz
-                eW1hbnRlY3NzbC5jb20wggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDf
-                klapgfE7MDlB++19m/I8TlzGJcIoiFUhqJN2TdCoiTPA+5eHkRRY9WWIS3R4xxQG
-                fA52dxynLRtce1Sr4zxeP5HkxtWKWbIdir2YVqnjWSoqyf0+8VNX4cYhCRu3BETu
-                Dfej5xjt7EH7++BoA5kGzcwv+7jb9U73XRREuZEq0l26QTd7EZjGZYATHJvz2idv
-                Z784+iGrO0Qw76rYHCnhffWDld9lgMKXcgRJpESIDQHRsPMJSREyWAOr0Fov/z75
-                YU3n4vXIFZSeSa7fGbyLFFXMNJpu4xG6x8JufgJkGZlgCEiX8aG6YjqV2Z3LrYld
-                4jzT408Uqyw2GftzZMDZAgMBAAGgADANBgkqhkiG9w0BAQUFAAOCAQEAPxIjS7g/
-                MUfNsRYuplgHh9BbZl13SVdFXc9POSJwSCy1pQhEhM+e7izGD3po+V0TlZ5DZohT
-                djrGMEZvBm4OkwB7g/9hzEI5kyHfBXzBVn9ybcIzEMlRqAWc0tS1Kn+EyyDlbGnh
-                iFEk178Q0KTuOIZfPVBKZjji0o5gj13wrRBAxJARf/0/MRqpg3mL932QgjmSB2dL
-                /57yk0hXh1ChA8d2htKdwb3RnRJHOjVxWbWjYGcuAMz7RTEN9pWviTM3y7FfTWTP
-                Q24Nrp12Ez1cALXb5t/lZkbrCtizCjUuEpREzIRMUYnWZEa7pw/CGbSTYH4a2x7n
-                L5mReDt1ijwjGg==
-                -----END CERTIFICATE REQUEST-----
-            """)
-        )
+    for order_id in orderids:
+        order_with_order_id(symantec, order_id)
 
     order_list = symantec.get_orders_by_date_range(**{
         "fromdate": date,
@@ -276,3 +234,16 @@ def test_get_orders_by_date_range(symantec):
     order_data = order_list.pop()
     assert order_data["PartnerOrderID"]
     assert order_data["OrderDate"]
+
+
+def test_get_order_by_partner_order_id(symantec):
+    order_id = "".join(random.choice(string.ascii_letters) for _ in range(30))
+
+    order_with_order_id(symantec, order_id)
+
+    order_data = symantec.get_order_by_partner_order_id(**{
+        "partnerorderid": order_id,
+        "partnercode": symantec.partner_code
+    })
+
+    assert order_data["OrderInfo"]["PartnerOrderID"] == order_id
